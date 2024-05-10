4fc976c0df739ff81aef84096a27168175b25890
Dat Dao <datdao@ariga.io>
2024-05-10 11:09:05 +0700

    src/commands: run migrate lint by atlasaction binary

--- src/scripts/echo.sh
+++ src/scripts/echo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "$@" >> /tmp/echo.out
+printenv >> /tmp/env.out
---
+++ src/scripts/migrate-lint-fail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# execute the command and check the exit code is 1
+# testing purpose only, use for integration tests
+atlasaction-copy --action migrate/lint
+if [ $? -ne 1 ]; then
+  echo "Expected exit code 1, got $?"
+  exit 1
+else
+  echo "Exit code 1 as expected"
+  exit 0
+fi
